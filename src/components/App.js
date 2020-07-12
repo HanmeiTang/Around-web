628fd2ca13cdb8eec3cdf74447e90381fd3bc891
Hanmei Tang <hmtang003@gmail.com>
2020-07-12 14:46:16 -0700

    update main

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import '../App.css';
+import React, {Component} from 'react';
+import TopBar from './TopBar';
+import '../styles/App.css';
 
-function App() {
-    return (
-        <div className="App">
-
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <TopBar/>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -7,6 +7,7 @@ class TopBar extends Component {
             <div>
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
+                    <span className="App-title">Around</span>
                     <p>
                         Edit <code>src/App.js</code> and save to reload.
                     </p>
