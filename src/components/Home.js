3c6b5429dc47d2ccb8cbbd4e678ccfe83e22b409
Hanmei Tang <hmtang003@gmail.com>
2020-07-15 22:09:39 -0700

    update home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,92 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {Tabs, Button, Spin} from 'antd';
+import {
+    GEO_OPTIONS,
+    POS_KEY,
+    API_ROOT,
+    AUTH_HEADER,
+    TOKEN_KEY
+} from '../constants'
 
-class Home extends Component {
+const {TabPane} = Tabs;
+
+class Home extends React.Component {
+    state = {
+        isLoadingGeoLocation: false,
+        isLoadingPosts: false,
+        error: '',
+        posts: [],
+    }
+
+    // When the component is rendered to the DOM for the first time
+    // such as at page load we call the Geolocation API to determine
+    // a latitude and longitude for the browser
+    componentDidMount() {
+        console.log(navigator.geolocation);
+        if ("geolocation" in navigator) {
+            this.setState({isLoadingGeoLocation: true, error: ''});
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS,
+            );
+        } else {
+            this.setState({error: 'Geolocation is not supported.'});
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position);
+        const {latitude, longitude} = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({lat: latitude, lon: longitude}));
+        this.setState({isLoadingGeoLocation: false, error: ''});
+        this.loadNearbyPosts();
+    }
+
+    onFailedLoadGeoLocation = () => {
+        this.setState({isLoadingGeoLocation: false, error: 'Failed to load geo location.'});
+    }
+
+    loadNearbyPosts = () => {
+        const {lat, lon} = JSON.parse(localStorage.getItem(POS_KEY));
+        const token = localStorage.getItem(TOKEN_KEY);
+        this.setState({isLoadingPosts: true, error: ''});
+        return fetch(`${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20000`, {
+            method: 'GET',
+            headers: {
+                Authorization: `${AUTH_HEADER} ${token}`
+            }
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Failed to load post.');
+            })
+            .then((data) => {
+                console.log(data);
+                this.setState({posts: data ? data : [], isLoadingPosts: false});
+            })
+            .catch((e) => {
+                console.error(e);
+                this.setState({isLoadingPosts: false, error: e.message});
+            });
+    }
+
     render() {
+        const operations = <Button type="primary">Create New Post</Button>;
         return (
-            <div>
-                This is Home
-            </div>
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Image Posts" key="1">
+                    {/*{this.renderImagePosts()}*/}
+                </TabPane>
+                <TabPane tab="Video Posts" key="2">
+                    Content of tab 2
+                </TabPane>
+                <TabPane tab="Map" key="3">
+                    Content of tab 3
+                </TabPane>
+            </Tabs>
         );
     }
 }
