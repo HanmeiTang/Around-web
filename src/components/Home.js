fd696360d5e4f0994214e19e5415cb09b757daa2
Hanmei Tang <hmtang003@gmail.com>
2020-07-20 19:02:25 -0700

    wrap up web 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,6 +13,7 @@ import {
 
 import Gallery from './Gallery';
 import CreatePostButton from './CreatePostButton';
+import AroundMap from "./AroundMap";
 
 const {TabPane} = Tabs;
 
@@ -138,7 +139,14 @@ class Home extends React.Component {
                     {this.renderPosts(POST_TYPE_VIDEO)}
                 </TabPane>
                 <TabPane tab="Map" key="3">
-                    Content of tab 3
+                    <AroundMap
+                        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3CEh9DXuyjozqptVB5LA-dN7MxWWkr9s&v=3.exp&libraries=geometry,drawing,places"
+                        loadingElement={<div style={{height: `100%`}}/>}
+                        containerElement={<div style={{height: `600px`}}/>}
+                        mapElement={<div style={{height: `100%`}}/>}
+                        posts={this.state.posts}
+                        loadNearbyPosts={this.loadNearbyPosts}
+                    />
                 </TabPane>
             </Tabs>
         );
